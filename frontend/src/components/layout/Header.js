17739730fd4ab752231db0d20d50490c1afc00ee
Meriem Chelli <meriem.chelli@etud.univ-jfc.fr>
2022-12-01 23:48:11 +0100

    fourth

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -24,22 +24,25 @@ const Header = () => {
   return (
     <Fragment>
       <nav className="navbar head  navbar-expand-lg ">
-        <div className="container">
-          <Link to="/" className="navbar-brand">
-            <img src={logo} style={{ height: "70px" }} className="px-2"></img>
-          </Link>
+        <div class="logoT">
+          <div className="container ">
+            <Link to="/" className="navbar-brand">
+              <img src={logo} style={{ height: "90px" }} className="px-2"></img>
+            </Link>
+            <h5 clas="textLogo">Question pour un champignon</h5>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
