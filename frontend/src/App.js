13c726e08ddd8287833cb6236adf7ef7f313c802
Meriem Chelli <meriem.chelli@etud.univ-jfc.fr>
2022-12-02 03:42:56 +0100

    999th

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,73 +1,85 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Header from './components/layout/Header'
-import Footer from './components/layout/Footer'
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
-import Home from './components/Home'
+import Home from "./components/Home";
 
 // Auth or User imports
-import Login from './components/user/Login'
-import Register from './components/user/Register'
-import Profile from './components/user/Profile'
-import UpdateProfile from './components/user/UpdateProfile'
-import UpdatePassword from './components/user/UpdatePassword'
-import ForgotPassword from './components/user/ForgotPassword'
-import NewPassword from './components/user/NewPassword'
-
-
+import Login from "./components/user/Login";
+import Register from "./components/user/Register";
+import Profile from "./components/user/Profile";
+import UpdateProfile from "./components/user/UpdateProfile";
+import UpdatePassword from "./components/user/UpdatePassword";
+import ForgotPassword from "./components/user/ForgotPassword";
+import NewPassword from "./components/user/NewPassword";
 
 // Admin Imports
-import UsersList from './components/admin/UsersList'
-import UpdateUser from './components/admin/UpdateUser'
+import UsersList from "./components/admin/UsersList";
+import UpdateUser from "./components/admin/UpdateUser";
 
 // home game & quiz
 
-import ProtectedRoute from './components/route/ProtectedRoute'
-import { loadUser } from './actions/userActions'
-import { useSelector } from 'react-redux'
-import store from './store'
-import QuizScreen from './components/QuizScreen'
-import TicTacToe from './components/TicTacToe'
-import Meme from './components/Meme'
+import ProtectedRoute from "./components/route/ProtectedRoute";
+import { loadUser } from "./actions/userActions";
+import { useSelector } from "react-redux";
+import store from "./store";
+import QuizScreen from "./components/QuizScreen";
+import TicTacToe from "./components/TicTacToe";
+import Meme from "./components/Meme";
 
-import './App.css'
-import AllGames from './components/AllGames'
-import Video from './components/Video'
+import "./App.css";
+import AllGames from "./components/AllGames";
+import Video from "./components/Video";
 
 function App() {
-	useEffect(() => {
-		store.dispatch(loadUser())
-	}, [])
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
 
-	const { user, isAuthenticated, loading } = useSelector((state) => state.auth)
+  const { user, isAuthenticated, loading } = useSelector((state) => state.auth);
 
-	return (
-		<Router>
-			<div className='App'>
-				<Header />
-				<div className='h-100'>
-					<Route path='/' component={Home} exact />
-					<Route path='/quiz' component={QuizScreen} exact />
-					<Route path='/game' component={TicTacToe} exact />
-					<Route path='/meme' component={Meme} exact />
-					<Route path='/video' component={Video} exact />
-					<Route path='/login' component={Login} />
-					<Route path='/register' component={Register} />
-					<Route path='/password/forgot' component={ForgotPassword} exact />
-					<Route path='/password/reset/:token' component={NewPassword} exact />
-					<ProtectedRoute path='/me' component={Profile} exact />
-					<ProtectedRoute path='/me/update' component={UpdateProfile} exact />
-					<ProtectedRoute path='/password/update' component={UpdatePassword} exact />
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="h-100">
+          <Route path="/" component={Home} exact />
+          <Route path="/quiz" component={QuizScreen} exact />
+          <Route path="/game" component={TicTacToe} exact />
+          <Route path="/meme" component={Meme} exact />
+          <Route path="/video" component={Video} exact />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/password/forgot" component={ForgotPassword} exact />
+          <Route path="/password/reset/:token" component={NewPassword} exact />
+          <ProtectedRoute path="/me" component={Profile} exact />
+          <ProtectedRoute path="/me/update" component={UpdateProfile} exact />
+          <ProtectedRoute
+            path="/password/update"
+            component={UpdatePassword}
+            exact
+          />
+          <ProtectedRoute path="/meme" component={Meme} exact />
+          <ProtectedRoute
+            path="/admin/users"
+            isAdmin={true}
+            component={UsersList}
+            exact
+          />
+          <ProtectedRoute
+            path="/admin/user/:id"
+            isAdmin={true}
+            component={UpdateUser}
+            exact
+          />
+        </div>
 
-					<ProtectedRoute path='/admin/users' isAdmin={true} component={UsersList} exact />
-					<ProtectedRoute path='/admin/user/:id' isAdmin={true} component={UpdateUser} exact />
-				</div>
-
-				{/* {!loading && (!isAuthenticated || user.role !== 'admin') && <Footer />} */}
-			</div>
-		</Router>
-	)
+        {/* {!loading && (!isAuthenticated || user.role !== 'admin') && <Footer />} */}
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Meme.js
+++ frontend/src/components/Meme.js
@@ -1,172 +1,120 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Card, Col, Row } from "react-bootstrap";
-import img1 from '../images/Meme/m1.jpg';
-import img2 from '../images/Meme/m2.jpg';
-import img3 from '../images/Meme/m3.jpg';
-import img4 from '../images/Meme/m4.jpg';
-import img5 from '../images/Meme/m5.jpg';
-import img6 from '../images/Meme/m6.jpg';
-import img7 from '../images/Meme/m7.jpg';
-import img8 from '../images/Meme/m8.jpg';
-import img9 from '../images/Meme/m9.jpg';
-import img10 from '../images/Meme/m10.jpg';
-import ReactCardCarousel from 'react-card-carousel';
+import img1 from "../images/Meme/m1.jpg";
+import img2 from "../images/Meme/m2.jpg";
+import img3 from "../images/Meme/m3.jpg";
+import img4 from "../images/Meme/m4.jpg";
+import img5 from "../images/Meme/m5.jpg";
+import img6 from "../images/Meme/m6.jpg";
+import img7 from "../images/Meme/m7.jpg";
+import img8 from "../images/Meme/m8.jpg";
+import img9 from "../images/Meme/m9.jpg";
+import img10 from "../images/Meme/m10.jpg";
 
 function Meme() {
-
   const [start, setStart] = useState(false);
 
-return (
+  return (
     <div className="container my-5">
-    <Row className="mt-5 row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
+      <Row className="mt-5 row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
         <Col className="mb-3">
           <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img1}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
+            <Card.Img
+              src={img1}
+              variant="top"
+              className="h-100"
+              style={{ height: "200px", borderRadius: "0px" }}
+            />
           </Card>
         </Col>
         <Col className="mb-3">
           <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img2}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
+            <Card.Img
+              src={img2}
+              variant="top"
+              className="h-100"
+              style={{ height: "200px", borderRadius: "0px" }}
+            />
           </Card>
         </Col>
         <Col className="mb-3">
           <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img3}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
+            <Card.Img
+              src={img3}
+              variant="top"
+              className="h-100"
+              style={{ height: "200px", borderRadius: "0px" }}
+            />
           </Card>
         </Col>
         <Col className="mb-3">
           <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img4}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
+            <Card.Img
+              src={img4}
+              variant="top"
+              className="h-100"
+              style={{ height: "200px", borderRadius: "0px" }}
+            />
           </Card>
         </Col>
         <Col className="mb-3">
           <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img5}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
+            <Card.Img
+              src={img5}
+              variant="top"
+              className="h-100"
+              style={{ height: "200px", borderRadius: "0px" }}
+            />
           </Card>
         </Col>
         <Col className="mb-3">
           <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img6}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
+            <Card.Img
+              src={img6}
+              variant="top"
+              className="h-100"
+              style={{ height: "200px", borderRadius: "0px" }}
+            />
           </Card>
         </Col>
         <Col className="mb-3">
           <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img7}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
+            <Card.Img
+              src={img7}
+              variant="top"
+              className="h-100"
+              style={{ height: "200px", borderRadius: "0px" }}
+            />
           </Card>
         </Col>
         <Col className="mb-3">
           <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img8}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
+            <Card.Img
+              src={img8}
+              variant="top"
+              className="h-100"
+              style={{ height: "200px", borderRadius: "0px" }}
+            />
           </Card>
         </Col>
         <Col className="mb-3">
           <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img9}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
+            <Card.Img
+              src={img9}
+              variant="top"
+              className="h-100"
+              style={{ height: "200px", borderRadius: "0px" }}
+            />
           </Card>
         </Col>
         <Col className="mb-3">
           <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img10}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
-          </Card>
-        </Col>
-        <Col className="mb-3">
-          <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img11}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
-          </Card>
-        </Col>
-        <Col className="mb-3">
-          <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img12}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
-          </Card>
-        </Col>
-        <Col className="mb-3">
-          <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img13}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
-          </Card>
-        </Col>
-        <Col className="mb-3">
-          <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img14}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
-          </Card>
-        </Col>
-        <Col className="mb-3">
-          <Card className="p-3 rounded h-100">
-              <Card.Img
-                src={img15}
-                variant="top"
-                className="h-100"
-                style={{ height: "200px", borderRadius: "0px" }}
-              />
+            <Card.Img
+              src={img10}
+              variant="top"
+              className="h-100"
+              style={{ height: "200px", borderRadius: "0px" }}
+            />
           </Card>
         </Col>
       </Row>
@@ -174,4 +122,4 @@ return (
   );
 }
 
-export default Meme ;
+export default Meme;
